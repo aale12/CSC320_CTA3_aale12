bb7a1fafe9e37e65783c738430572d8f5ed3078c
Anthony Le <leanthony104@yahoo.com>
2024-06-30 15:21:38 -0500

    code done

--- CTA3_Option1.java
+++ CTA3_Option1.java
@@ -1,3 +1,5 @@
+package CSC320.CSC320_CTA3_aale12;
+
 import java.util.Scanner; //Import the Scanner class
 
 public class CTA3_Option1 {
@@ -7,13 +9,26 @@ public class CTA3_Option1 {
 
         double userIncome = scnr.nextDouble();
         double tax = 0;
-        double total = userIncome + tax;
-        System.out.println("Inputted weekly income is:" + userIncome);
+        int taxBracket = 10;
+        //System.out.println("Inputted weekly income is:" + userIncome);
 
         if (userIncome < 500) {
             tax = userIncome * 0.10;
+            //taxBracket = 10;
         } else if (userIncome >= 500 && userIncome < 1500) {
             tax = userIncome * 0.15;
+            taxBracket = 15;
+        } else if (userIncome >= 1500 && userIncome <2500) {
+            tax = userIncome * 0.20;
+            taxBracket = 20;
+        } else if (userIncome >= 2500) {
+            tax = userIncome * 0.30;
+            taxBracket = 30;
         }
+
+        double total = userIncome - tax;
+        System.out.println("The weekly tax for an income of $" + String.format("%.2f", userIncome) + " is $" + String.format("%.2f", tax) + " (" + taxBracket + "%)");
+        System.out.println("The total amount after tax is $" + String.format("%.2f", total));
+        scnr.close();
     }
 }
